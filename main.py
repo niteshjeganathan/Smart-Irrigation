32f81b9ae9bcb4e662547883fa8d7d00b2dfaa3c
Nitesh Jeganathan <pen2nitesh@gmail.com>
2024-09-09 18:49:16 +0530

    Preprocessing

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-# Importing Libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
 # Importing Dataset
 dataset = pd.read_csv('data.csv', skiprows=5)
 X = dataset.iloc[:, [1, 2, 5, 6, 9, 11]].values
+
+# Calculating Average Temperature (average of the first two columns)
+avg_temp = np.round(np.mean(X[:, [0, 1]], axis=1), 1)
+
+# Calculating Average Humidity (average of the next two columns, 5 and 6)
+avg_humidity = np.round(np.mean(X[:, [2, 3]], axis=1), 1)
+
+# Now update the array, keeping the averaged temperature and humidity
+X = np.column_stack((avg_temp, avg_humidity, X[:, 4:]))
+
+print(X)
